db5771c5db8162d9a5cec588b191e7170a340dad
Liang xinzhong <johnson.liangxinzhong@gmail.com>
2017-04-08 18:00:35 +0800

    add vote, sort, about function

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Hello World!'
+puts "這個種子檔會自動建立 10 個 topics"
+
+create_topics = for i in 1..10 do
+  Topic.create!([title: "Topic no.#{i}", description: "這是用種子建立的第 #{i} 個主题" ])
+end
